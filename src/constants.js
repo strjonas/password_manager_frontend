e8d32b32cac16b4da3fa1285a1eedfe1d07eff59
strjonas <jonas.strabel@gmail.com>
2021-07-27 15:37:26 +0200

    dljdas

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const REACT_APP_API =
   process.env.NODE_ENV === "development"
-    ? "http://192.168.178.41:5000"
-    : "https://webapi.herokuapp.com";
+    ? "https://localwebapi.herokuapp.com"
+    : "https://localwebapi.herokuapp.com";
